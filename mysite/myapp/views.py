8b7835d8854d4ce007ddcddb6b7828b45bb1b755
smruti-ranjan-kodamasingh <kodamasinghsmrutiranjan2003@gmail.com>
2024-05-28 12:46:43 +0530

    second commit

---
+++ mysite/myapp/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render,redirect
+from .models import Task
+from .forms import TodoForm
+
+# Create your views here.
+
+
+def index(request):
+    task_list = Task.objects.all()
+    if request.method == "POST":
+        name = request.POST.get('name','')
+        priority = request.POST.get('priority','')
+        date = request.POST.get('date','')
+    
+        task = Task(name = name, priority = priority, date = date)
+        task.save()
+        return redirect('/')
+
+    return render(request, 'myapp/index.html',{'task_list':task_list})
+
+def delete(request,taskid):
+    task = Task.objects.get(id=taskid)
+    if request.method == "POST":
+        task.delete()
+        return redirect('/')
+    return render(request,'myapp/delete.html',{'task':task})
+
+def update(request, id):
+    task = Task.objects.get(id=id)
+    form = TodoForm(request.POST or None, instance=task)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    return render(request, 'myapp/edit.html', {'form':form,'task':task})
