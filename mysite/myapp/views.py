3298b0abf8d3a5e2c425ebe658c59327a57f63f5
smruti-ranjan-kodamasingh <kodamasinghsmrutiranjan2003@gmail.com>
2024-05-29 03:23:28 +0530

    generic class views

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -3,6 +3,8 @@ from .models import Task
 from .forms import TodoForm
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView,DeleteView
+from django.urls import reverse_lazy
 
 # Create your views here.
 class TaskListView(ListView):
@@ -14,6 +16,20 @@ class TaskDetailView(DetailView):
     model = Task
     template_name = 'myapp/detail.html'
     context_object_name = 'task'
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    template_name = 'myapp/update.html'
+    context_object_name = 'task'
+    fields = ('name','priority','date')
+
+    def get_success_url(self) -> str:
+        return reverse_lazy('cbvdetail',kwargs={'pk':self.object.id})
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name = 'myapp/delete.html'
+    success_url = reverse_lazy('cbvindex')   
 
 def index(request):
     task_list = Task.objects.all()
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     path("update/<int:id>/",views.update,name='update'),
     path("cbvindex/",views.TaskListView.as_view(),name='cbvindex'),
     path("cbvdetail/<int:pk>/",views.TaskDetailView.as_view(),name='cbvdetail'),
+    path("cbvupdate/<int:pk>/",views.TaskUpdateView.as_view(),name='cbvupdate'),
+    path("cbvdelete/<int:pk>/",views.TaskDeleteView.as_view(),name='cbvdelete'),
 ]
